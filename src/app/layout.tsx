8172f00a26abcb0c6974eac2d3a378dc09c57700
filipe <felepesilvagalvao@gmail.com>
2026-09-13 15:38:40 -0300

    adição da tag analitcs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { siteMetadata } from "../lib/seo";
+import Script from "next/script";
 
 export const metadata: Metadata = siteMetadata;
 
@@ -19,6 +20,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-78D6C1Y7ZD"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-78D6C1Y7ZD');
+          `}
+        </Script>
+      </head>
       <body>
         <div className="app-shell">{children}</div>
       </body>
