2b56a790cbd3b8a5c41064ba4a730cf18dbca6e4
filipe <felepesilvagalvao@gmail.com>
2026-09-12 21:52:44 -0300

    force-dynamic on sitemap and robots metadata routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 import { getLatestMovies, getLatestSeries } from "../services/catalog";
 import { absoluteUrl } from "../lib/seo";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const [movies, series] = await Promise.all([
     getLatestMovies(500),
